3108a3d1aaeae39552aa47353ab0bf7ba15ac406
O F <teleclimber@gmail.com>
2025-12-31 11:01:43 -0800

    feat: time bar improvements
    - fixes #3

--- frontend/src/components/TimeBar.vue
+++ frontend/src/components/TimeBar.vue
@@ -11,14 +11,14 @@ const cur_year = cur_date.getFullYear();
 const cur_month = cur_date.getMonth();
 
 const months :{year:number, month:number}[] = [];
-
+months.push( {year, month} );
 while(true) {
-	months.push( {year, month} );
 	++month;
 	if( month === 12 ) {
 		month = 0;
 		++year;
 	}
+	months.push( {year, month} );
 	if( year === cur_year && month === cur_month ) break;
 }
 
@@ -31,16 +31,38 @@ function monthClicked(event:Event, month:{year:number, month:number}) {
 	d.setDate(1);
 	noteStackStore.goToDate(d);
 }
+function endClicked() {
+	const d = new Date;
+	d.setDate(d.getDate() + 1);
+	noteStackStore.goToDate(d);
+}
 
 </script>
 
 <template>
-	<div class="bg-slate-50 h-full absolute top-0 right-0 overflow-x-scroll" style="width:50px;">
-		<div>
-			<div v-for="m in months" class="h-12 text-center" @click="monthClicked($event, m)">
-				<span v-if="m.month === 0" class="block text-sm font-bold text-gray-400">{{ m.year }}</span>
-				<span class="block uppercase text-sm text-gray-500">{{ month_strs[m.month] }}</span>
+	<div class="bg-slate-50 h-full absolute top-0 right-0 flex flex-col">
+		<div class=" overflow-x-scroll ill-bottom-scroll" style="width:50px;">
+			<div>
+				<template v-for="m in months">
+					<div v-if="m.month === 0" class="block text-center text-sm font-bold bg-gray-500 text-gray-50">{{ m.year }}</div>
+					<a href="#" class="h-12 border-t border-gray-300 flex items-center justify-center hover:bg-yellow-200" @click.stop.prevent="monthClicked($event, m)">
+						<span class="uppercase text-sm text-gray-500">{{ month_strs[m.month] }}</span>
+					</a>
+				</template>
 			</div>
 		</div>
+		<a href="#" class="h-12 text-center bg-gray-500 hover:bg-yellow-500 text-gray-50 py-2" @click.stop.prevent="endClicked">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 inline">
+				<path stroke-linecap="round" stroke-linejoin="round" d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5" />
+			</svg>
+		</a>
 	</div>
 </template>
+
+<style scoped>
+/* From https://stackoverflow.com/questions/18614301/keep-overflow-div-scrolled-to-bottom-unless-user-scrolls-up */
+.ill-bottom-scroll {
+	display: flex;
+	flex-direction: column-reverse;
+}
+</style>
